997dda870d857dd9bbb763dd669a272deda1dd1a
KUNPENG1 <616574100@qq.com>
2020-05-24 23:22:43 +0800

    [add]week_01作业提交

---
+++ Week_01/src/week_01.java
@@ -0,0 +1,90 @@
+package src;
+
+import java.util.*;
+
+public class week_01 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 1. 两数之和
+     * <p>
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * <p>
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     */
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(nums.length);
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(target - nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    /**
+     * 11. 盛最多水的容器
+     * <p>
+     * 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     * <p>
+     * 说明：你不能倾斜容器，且 n 的值至少为 2。
+     */
+    public static int maxArea(int[] height) {
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j; ) {
+            int minHeight = height[i] < height[j] ? height[i++] : height[j--];
+            max = Math.max(max, (j - i) * minHeight);
+        }
+        return max;
+    }
+
+    /**
+     * 15. 三数之和
+     * <p>
+     * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+     * <p>
+     * 注意：答案中不可以包含重复的三元组。
+     */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (nums.length < 3) {
+            return lists;
+        }
+        Arrays.sort(nums);
+        int l, r;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) {
+                break;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            l = i + 1;
+            r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    lists.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    l++;
+                    r--;
+                } else if (sum > 0) {
+                    r--;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return lists;
+    }
+}
